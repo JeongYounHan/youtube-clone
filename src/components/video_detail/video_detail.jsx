4a1dfd12ff070a09a25ab721a931bdb3d6159432
JeongYounHan <51sarang@hanmail.net>
2022-02-25 04:06:15 +0900

    video detail 컴포넌트 추가

---
+++ src/components/video_detail/video_detail.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./video_detail.module.css";
+
+const VideoDetail = ({ video }) => {
+  return (
+    <section className={styles.detail}>
+      <iframe className={styles.video} src={`https://www.youtube.com/embed/${video.id}`} type="text/html" width="100%" height="500px" frameBorder="0" allowFullScreen></iframe>
+    </section>
+  );
+};
+
+export default VideoDetail;

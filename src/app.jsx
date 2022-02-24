3c37021ce10d416988c22d2f1f9af5096a626776
JeongYounHan <51sarang@hanmail.net>
2022-02-25 02:05:55 +0900

    api class 따로 만들어서 dependency injection

--- src/app.jsx
+++ src/app.jsx
@@ -4,32 +4,20 @@ import { useEffect } from "react";
 import VideoList from "./components/video_list/video_list";
 import SearchHeader from "./components/search_header/search_header";
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
   const search = (query) => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=25&q=${query}&key=AIzaSyBB2wQWDQd4VOrpBFJyaYr-oE8yUsiWoh4`, requestOptions)
-      .then((response) => response.json())
-      .then((result) => result.items.map((item) => ({ ...item, id: item.id.videoId })))
-      .then((items) => setVideos(items))
-      .catch((error) => console.log("error", error));
+    youtube
+      .search(query) //
+      .then((videos) => setVideos(videos));
   };
 
   useEffect(() => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBB2wQWDQd4VOrpBFJyaYr-oE8yUsiWoh4", requestOptions)
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log("error", error));
+    youtube
+      .mostPopular() //
+      .then((videos) => setVideos(videos));
   }, []); // mount가 될때만 호출
+
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />

2abfa35f5f98d614804f08b976b6230b427c417a
JeongYounHan <51sarang@hanmail.net>
2022-02-24 20:45:37 +0900

    youtube api 사용해서 컴포넌트에 뿌려주기

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,23 @@
 import "./app.css";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import VideoList from "./components/video_list/video_list";
 
 function App() {
-  return <h1>Hello^^</h1>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBB2wQWDQd4VOrpBFJyaYr-oE8yUsiWoh4", requestOptions)
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []); // mount가 될때만 호출
+  return <VideoList videos={videos} />;
 }
 
 export default App;
